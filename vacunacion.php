331583c5b4c9fabe560da522b4238878fce75a4e
juanbalmes <82063640+juanbalmes@users.noreply.github.com>
2021-04-06 17:30:50 -0500

    Add files via upload

---
+++ vacunacion.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html>
+<head>
+ <title>Formulario de datos</title>
+</head>
+
+<H1 align="center"><img src="descarga.png">
+<body>
+  <center><h1> <p style="color:rgb(100,230,222);">Completa el formulario de vacunacion</p> </h1></center>
+
+<form action="Formulario" method="get">
+  
+  <H1 align="left"><p>Nombre: <input type="text" name="nombre" size="40" planceholder="escribasunombre"></p>                
+  <p>Apellidos:
+  <input type="text" name="apellidos" size="40" placeholder="Escriba sus dos apellidos">
+</p>
+  <p>Fecha de nacimiento:<input type="date" name="fecha">
+  <p>Sexo:
+    <input type="radio" name="hm" value="h"> Hombre
+    <input type="radio" name="hm" value="m"> Mujer
+    <p>Nacionalidad: <select name="menu">
+  <option value="1">afgano</option>
+  <option value="2">aleman</option>
+  <option value="3">arabe</option>
+  <option value="4">argentino</option>
+  <option value="5">australiano</option>
+  <option value="6">belga</option>
+  <option value="7">boliviano</option>
+  <option value="8">brasileno</option>
+  <option value="9">camboyano</option>
+  <option value="10">canadiense</option>
+  <option value="11">chileno</option>
+  <option value="12">chino</option>
+  <option value="13">colombiano</option>
+  <option value="14">coreano</option>
+  <option value="15">costarricense</option>
+  <option value="16">cubano</option>
+  <option value="17">ecuatoriano</option>
+  <option value="18">egipcio</option>
+  <option value="19">salvadoreno</option>
+  <option value="20">escoces</option>
+  <option value="21">espanol</option>
+  <option value="22">estadounidense</option>
+  <option value="23">estonio</option>
+  <option value="24">etiope</option>
+  <option value="25">filipino</option>
+  <option value="26">finlandes</option>
+  <option value="27">frances</option>
+  <option value="28">gales</option>
+  <option value="29">griego</option>
+  <option value="30">guatemalteco</option>
+  <option value="31">haitiano</option>
+  <option value="32">holandes</option>
+  <option value="33">hondureno</option>
+  <option value="34">indones</option>
+  <option value="35">ingles</option>
+  <option value="36">iraqui</option>
+  <option value="37">irani</option>
+  <option value="38">irlandes</option>
+  <option value="39">israeli</option>
+  <option value="40">italiano</option>
+  <option value="41">japones</option>
+  <option value="42">jordano</option>
+  <option value="43">laosiano</option>
+  <option value="44">leton</option>
+  <option value="45">letones</option>
+  <option value="46">malayo</option>
+  <option value="47">marroqui</option>
+  <option value="48">mexicano</option>
+  <option value="49">nicaraguense</option>
+  <option value="50">noruego</option>
+  <option value="51">neozelandes</option>
+  <option value="52">panameno</option>
+  <option value="53">paraguayo</option>
+  <option value="54">peruano</option>
+  <option value="55">polaco</option>
+  <option value="56">portugues</option>
+  <option value="57">puertorriqueno</option>
+  <option value="58">dominicano</option>
+  <option value="59">rumano</option>
+  <option value="60">ruso</option>
+  <option value="61">sueco</option>
+  <option value="62">suizo</option>
+  <option value="63">tailandes</option>
+  <option value="64">taiwanes</option>
+  <option value="65">turco</option>
+  <option value="66">ucraniano</option>
+  <option value="67">uruguayo</option>
+  <option value="68">venezolano</option>
+  <option value="69">vietnamita</option>
+</select>
+<H1 align="justify"><p>Introdusca su curp: <input type="text" name="nombre"></p>
+    <a href="https://www.gob.mx/curp/"><input type="button" value="Consulte curp"></a>
+    <p>Municipio en el que radicas: <input type="text" name="Direccion" maxlength="6"></p>
+    <p>Colonia: <input type="text" name="colonia" maxlength="6"></p>
+    <p>calle: <input type="text" name="calle" maxlength="6"></p>
+    <p>Numero interior: <input type="number" name="numero">
+    <p>Numero exterior: <input type="number" name="numero">
+    <p>Codigo postal: <input type="number" name="numero">
+    <p>Referencia: <input type="text" name="referencia" maxlength="6"></p>
+    Telefono: <input type="tel" name="telefono">
+    <p> ¿ Ustes a pedesido de covid 19 ? 
+
+<p>
+  <input type="radio" name="boton" value="1">Si<br>
+  <input type="radio" name="boton" value="2">No<br>
+  <input name="Submit" type="submit" value="Enviar" />
+</form>
+  <form action="juanbalmes@hotmail.com"></form>
+
+</p>
+  </p>
+</form>
